c31322b23eadaa70a199bf61f662f0f30169426e
Yohanes <yohanesmulugeta21@gmail.com>
2023-05-13 16:52:43 +0300

    implement deletion.

--- public/js/chat/renderChats.js
+++ public/js/chat/renderChats.js
@@ -1,5 +1,8 @@
 import { setCurrentChat } from '../scriptDemo.js';
+import { removeProgress, showProgress } from '../resusables/showProgressBtn.js';
 import Chat from './chat.js';
+import showError from '../resusables/showError.js';
+import makeRequest from '../resusables/fetch.js';
 
 const sideBar = document.querySelector('.chat-column-left-row-one');
 const loaderChatBtn = document.querySelector('.loader-chatbtns ');
@@ -18,8 +21,8 @@ export default function renderChatBtns(e) {
     sideBar.insertAdjacentHTML(
       'beforeend',
       `
-      <div class='chat-btn-delete-container'>
-        <button data-docname=${chat[0]} data-chattitle=${chat[1].chatTitle} class='btn-sample-pdf btn btn-primary btn-chat'>
+      <div class='chat-btn-delete-container' data-docname=${chat[0]} data-chattitle=${chat[1].chatTitle}>
+        <button class='btn-sample-pdf btn btn-primary btn-chat'>
           <i class='bi bi-file-earmark-pdf'></i> ${chat[1].chatTitle}
         </button>
         <button class='btn-danger btn btn-delete-chat'>
@@ -30,9 +33,38 @@ export default function renderChatBtns(e) {
   });
 }
 
+async function handleDeleteChat(e) {
+  const targetBtn = e.target.closest('.btn-delete-chat');
+  const { docname } = targetBtn.closest('.chat-btn-delete-container').dataset;
+  try {
+    let time = 3;
+    targetBtn.classList.remove('btn-delete-chat');
+    const handleInterval = () => {
+      console.log('lala');
+      targetBtn.innerHTML = `<i class="bi bi-arrow-90deg-left">${time}</i>`;
+      time--;
+    };
+
+    handleInterval();
+    const intervalId = setInterval(handleInterval, 1000);
+    const timeoutId = setTimeout(deleteChat, time * 1100, targetBtn, docname, intervalId);
+
+    // binding the intercal id an dtarget btn to the undo handler
+    const bindOpt = { intervalId, targetBtn, timeoutId };
+    const handler = handleUndo.bind(bindOpt);
+    bindOpt.handler = handler;
+
+    targetBtn.addEventListener('click', handler);
+  } catch (err) {
+    showError(err, targetBtn, `<i class='bi bi-archive'></i> `);
+  }
+}
+
 export function handleChatBtns(e) {
   const chatBtn = e.target.closest('.btn-chat');
-  if (!chatBtn) return;
+  const deleteChatBtn = e.target.closest('.btn-delete-chat');
+  if (!chatBtn && !deleteChatBtn) return;
+  if (deleteChatBtn) return handleDeleteChat(e);
 
   // making previous chatbtn available
   const prevActiveBtn = document.querySelector('.active-chat-btn');
@@ -43,6 +75,33 @@ export function handleChatBtns(e) {
   chatBtn.classList.add('active-chat-btn');
   chatBtn.setAttribute('disabled', true);
 
-  const chat = new Chat(chatBtn.dataset.chattitle, chatBtn.dataset.docname);
+  const chat = new Chat(
+    chatBtn.closest('.chat-btn-delete-container').dataset.chattitle,
+    chatBtn.closest('.chat-btn-delete-container').dataset.docname
+  );
   setCurrentChat(chat);
+}
+
+// ///////////// //
+//    HELEPERS  //
+//  ////////// //
+
+function handleUndo(e) {
+  // garbage collection
+  this.targetBtn.removeEventListener('click', this.handler);
+  this.targetBtn.innerHTML = `<i class='bi bi-archive'></i>`;
+  clearInterval(this.intervalId);
+  clearTimeout(this.timeoutId);
+
+  // adding the class list after the event has bubbled already
+  setTimeout(() => {
+    this.targetBtn.classList.add('btn-delete-chat');
+  }, 1000);
+}
+
+async function deleteChat(btn, docname, intervalId) {
+  clearInterval(intervalId);
+  showProgress(btn);
+  await makeRequest({ method: 'delete', url: `/api/v1/pdf/${docname}` });
+  removeProgress(btn, `<i class='bi bi-archive'></i>`);
 }
