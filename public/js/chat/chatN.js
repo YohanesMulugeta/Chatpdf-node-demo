466150123175ecdb64f51753b20048d8f74555e0
Yohanes <yohanesmulugeta21@gmail.com>
2023-08-29 22:50:29 +0300

    update teh ws to wss.

--- public/js/chat/chatN.js
+++ public/js/chat/chatN.js
@@ -24,7 +24,7 @@ class Chat {
     this.state.docName = chatTitle;
     this.state.history = chatHistory ? chatHistory : [];
     this.state.chatId = _id;
-    this.url = `ws://${location.hostname}${
+    this.url = `wss://${location.hostname}${
       location.port ? ':' + location.port : ''
     }/api/v1/chat/${_id}`;
     // this.url = `wss://${
